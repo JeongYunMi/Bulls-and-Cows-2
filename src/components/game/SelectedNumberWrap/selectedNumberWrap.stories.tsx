6d0b9b0f89777e640f1b041efd84a54de0a9e144
JeongYunMi <tamd5971@gamil.com>
2023-05-24 16:02:54 +0900

    fix: storybook 수정

--- src/components/game/SelectedNumberWrap/selectedNumberWrap.stories.tsx
+++ src/components/game/SelectedNumberWrap/selectedNumberWrap.stories.tsx
@@ -1,17 +1,44 @@
 import SelectedNumberWrap from "./selectedNumberWrap";
-import { Story, Meta } from '@storybook/react';
-import { SelectedNumberProps } from '../../../types/content';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
+
+const meta: Meta<typeof SelectedNumberWrap> = {
   title: 'game/SelectedNumberWrap',
   component: SelectedNumberWrap,
-} as Meta;
+  tags: ['autodocs'],
+  argTypes: {
+    selectedNumber: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '',
+    },
+    deleteHandler: {
+      control: {
+        type: 'function',
+      },
+      defaultValue: ()=>{},
+    },
+    sendButtonHandler: {
+      control: {
+        type: 'function',
+      },
+      defaultValue: ()=>{},
+    },
+  },
+  parameters: {
+    componentSubtitle: '선택된 숫자 컴포넌트 문서입니다.'
+  }
+};
 
-const Template: Story<SelectedNumberProps> = (args) => <SelectedNumberWrap {...args} />;
+export default meta;
 
-export const Default = Template.bind({});
-Default.args = {
-  selectedNumber: "123",
-  deleteHandler: () => {},
-  sendButtonHandler: () => {},
+type Story = StoryObj<typeof SelectedNumberWrap>;
+
+export const option1: Story = {
+  args: {
+    selectedNumber: '123',
+    sendButtonHandler: ()=>{},
+    deleteHandler: () => {}
+  },
 };
